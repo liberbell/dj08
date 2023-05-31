b14ba90f1be7751caa4486ef4f144f3f68a64405
Liberbell <liberbell@gmail.com>
2023-06-01 08:53:55 +0900

    sample33

--- sample3.py
+++ sample3.py
@@ -3,8 +3,11 @@ is_animal = False
 if is_animal:
     print("It`s an animal")
 
-is_man = True
-is_adult = True
+is_man = False
+is_adult = False
 
 if is_man or is_adult:
     print("man or adult")
+
+if is_man and is_adult:
+    print("adult man")
