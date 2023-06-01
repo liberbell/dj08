9805d93b97ea6003d0dc36c9c4121cb3f014daa4
Liberbell <liberbell@gmail.com>
2023-06-02 08:39:17 +0900

    sample45

--- sample4.py
+++ sample4.py
@@ -1,13 +1,18 @@
 value = 1
-print(value)
-value = -1
-print(value)
+# print(value)
+# value = -1
+# print(value)
 
-value = value + 3
-print(value)
-print(value * 4)
-print(value / 3)
+# value = value + 3
+# print(value)
+# print(value * 4)
+# print(value / 3)
 
-value = 10
-print(value // 3)
-print(value % 3)
+# value = 10
+# print(value // 3)
+# print(value % 3)
+
+value += 3
+print(value)
+value -= 2
+print(value)
