12afdf290a4e3c4f084cfe4661a5559d50885793
Liberbell <liberbell@gmail.com>
2023-06-05 08:45:43 +0900

    sample97

--- sample9.py
+++ sample9.py
@@ -1,13 +1,17 @@
-list_a = [1, 2, 3, 4]
-list_b = []
+# list_a = [1, 2, 3, 4]
+# list_b = []
 
-print(list_a, list_b)
-print(list_a[0], list_b)
+# print(list_a, list_b)
+# print(list_a[0], list_b)
 
-list_a = [1, [1, 2, "apple"], 3, "banana"]
-print(list_a[1][2])
+# list_a = [1, [1, 2, "apple"], 3, "banana"]
+# print(list_a[1][2])
 
-list_a[1][2] = "lemon"
-print(list_a)
-list_a.reverse()
-print(list_a)
+# list_a[1][2] = "lemon"
+# print(list_a)
+# list_a.reverse()
+# print(list_a)
+
+list_a = [1, 2, 3, 4, 5]
+list_b = list_a[:3]
+print(list_b)
