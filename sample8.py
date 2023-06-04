146f03011f868ba8810020c12cfb2e5ae0b9440f
Liberbell <liberbell@gmail.com>
2023-06-05 08:25:45 +0900

    sample81

--- sample8.py
+++ sample8.py
@@ -6,3 +6,9 @@ add_string = sum_string + int_to_str
 float_num = 20.5
 str_float = str(float_num)
 print(type(str_float))
+
+msg = "12"
+int_msg = int(msg)
+float_msg = float(msg)
+print(type(int_msg))
+print(type(float_msg))
