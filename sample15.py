312a58faf8de0aad786b899e33b6d149e0b19bc2
Liberbell <liberbell@gmail.com>
2023-06-09 07:47:45 +0900

    sample153

--- sample15.py
+++ sample15.py
@@ -1,4 +1,15 @@
-var = False
+
+class ClassA:
+    def __init__(self, a):
+        self.a = a
+
+    def __bool__(self):
+        if self.a == "A":
+            return True
+        else:
+            return False
+
+var = []
 print("bool calculation:{}".format(bool(var)))
 if var:
     print("inside if route")
