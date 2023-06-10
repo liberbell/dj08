12f2dddfd399e576d7f2b79b22db3b727d31e2ff
Liberbell <liberbell@gmail.com>
2023-06-10 14:51:46 +0900

    sample172

--- sample17.py
+++ sample17.py
@@ -1,3 +1,5 @@
-
-if any((30 < 40, 10 < 20, "a" == "a")):
+if all((30 < 40, 10 < 20, "a" == "a")):
     print("inside all function")
+
+if any((30 < 40, 10 < 20, "a" == "b")):
+    print("inside any function")
