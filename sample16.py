a5fe223785ac52786361f12f0562eddc5e71014d
Liberbell <liberbell@gmail.com>
2023-06-10 14:41:52 +0900

    sample163

--- sample16.py
+++ sample16.py
@@ -8,10 +8,18 @@ elif msg == "red":
 else:
     print("warning")
 
-age = 61
+age = 1
 if age < 20:
     print("under 20")
 elif age <= 40:
     print("under 40 and over 20")
 elif age >= 60:
     print("over 60")
+else:
+    print("some age.")
+
+gender = "male"
+age = 10
+
+if gender == "male" and age < 20:
+    print("under 20 male")
