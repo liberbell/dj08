1fac7ae73f539fa2a20bef1e01490146202df271
Liberbell <liberbell@gmail.com>
2023-06-11 16:24:56 +0900

    sample213

--- sample21.py
+++ sample21.py
@@ -1,9 +1,17 @@
 import traceback
 
 try:
+    b = [10, 20, 30]
+    a = b[4]
     a = 10 / 0
 except ZeroDivisionError as e:
     # print(e, type(e))
     traceback.print_exc()
     pass
+
+except IndexError as e:
+    print("index error")
+
+except Exception as e:
+    print("error", e)
 print("end of calculation")
