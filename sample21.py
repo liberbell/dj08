eebfec44b3477f83056ba30f1a359fc76562582c
Liberbell <liberbell@gmail.com>
2023-06-11 16:22:26 +0900

    sample213

--- sample21.py
+++ sample21.py
@@ -1,7 +1,9 @@
+import traceback
 
 try:
     a = 10 / 0
 except ZeroDivisionError as e:
-    print(e, type(e))
+    # print(e, type(e))
+    traceback.print_exc()
     pass
 print("end of calculation")
