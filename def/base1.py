fa6c0a39aec919755d4a9e1869caf0c9f142ccb8
Liberbell <liberbell@gmail.com>
2023-06-12 08:48:45 +0900

    base12

--- def/base1.py
+++ def/base1.py
@@ -3,3 +3,10 @@ def print_hello():
     print("Hello World")
 
 print_hello()
+
+def num_max(a, b):
+    print("a: {}, b: {}".format(a, b))
+    if a > b:
+        return a
+    else:
+        return b
