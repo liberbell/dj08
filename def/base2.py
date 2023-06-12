524bea1a000a838dfe0ef90cbc0039844493ef1e
Liberbell <liberbell@gmail.com>
2023-06-12 09:06:06 +0900

    base26

--- def/base2.py
+++ def/base2.py
@@ -17,3 +17,8 @@ def spam2(arg1, **arg2):
     print(type(arg2))
 
 spam2(3, name="Bob", age=20)
+
+def spam3(arg1, *arg2, **arg3):
+    print("arg1: {}, arg2: {}, arg3: {}".format(arg1, arg2, arg3))
+
+spam3(1, 2, 3, 4, 5, name="Bob", age=23)
