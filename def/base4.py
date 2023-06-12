6ea9cbd42c6f0c5d5c2c0e8b7652fef503977a38
Liberbell <liberbell@gmail.com>
2023-06-13 08:47:25 +0900

    base46

--- def/base4.py
+++ def/base4.py
@@ -6,7 +6,6 @@ def outer():
     def inner():
         outer_value = "inner outer valuables"
         print("inner outer value={}, id={}".format(outer_value, id(outer_value)))
-        print("inner print")
 
     inner()
     print("outer value={}, id={}".format(outer_value, id(outer_value)))
