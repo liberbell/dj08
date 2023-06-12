b34eb0984465d2bcdb8b63f4a80102fe9496ede5
Liberbell <liberbell@gmail.com>
2023-06-13 08:48:48 +0900

    base46

--- def/base4.py
+++ def/base4.py
@@ -4,6 +4,7 @@ def outer():
     outer_value = "outer valuables"
 
     def inner():
+        nonlocal outer_value
         outer_value = "inner outer valuables"
         print("inner outer value={}, id={}".format(outer_value, id(outer_value)))
 
