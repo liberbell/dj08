db56b10bd1af1fa89c74df459833e020c15f12d8
Liberbell <liberbell@gmail.com>
2023-06-15 08:31:40 +0900

    base32

--- excesise/base3.py
+++ excesise/base3.py
@@ -1,2 +1,11 @@
 
-hand_dict = ["1": "rock", "2": "scissors", "3": "paper"]
+def generator_enemy_hand():
+    while True:
+        yield "1"
+        yield "2"
+        yield "3"
+
+hand_dict = {"1": "rock", "2": "scissors", "3": "paper"}
+
+lose_count = 0
+enemy_hands = generator_enemy_hand()
