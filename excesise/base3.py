cb2020ce995114c7b3dcf36d4dde26e332ebfaea
Liberbell <liberbell@gmail.com>
2023-06-15 08:39:25 +0900

    base36

--- excesise/base3.py
+++ excesise/base3.py
@@ -5,7 +5,17 @@ def generator_enemy_hand():
         yield "2"
         yield "3"
 
-hand_dict = {"1": "rock", "2": "scissors", "3": "paper"}
+def is_win(my_hand, enemy_hand):
+    if my_hand == "1" and enemy_hand == "2":
+        return True
+    elif my_hand == "1" and enemy_hand == "3":
+        return False
+
+hand_dict = {
+    "1": "rock",
+    "2": "scissors",
+    "3": "paper"
+    }
 
 lose_count = 0
 enemy_hands = generator_enemy_hand()
@@ -17,3 +27,4 @@ while True:
         continue
     enemy_hand = next(enemy_hands)
     print("Your hand: {}. Computer hand: {}".format(hand_dict.get(my_hand), hand_dict.get(enemy_hand)))
+    if my_hand == enemy_hand
