1d694a3e1350b542040f88c58199ff455513f1a4
Liberbell <liberbell@gmail.com>
2023-06-15 08:59:01 +0900

    base85

--- def/base8.py
+++ def/base8.py
@@ -1,11 +1,17 @@
 
 
+# def print_hello():
+#     print("Hello")
+
+# def print_goodbye():
+#     print("Goodbye")
+
+# var = ["AA", "BB", print_hello, print_goodbye]
+# var[2]()
+# var[3]()
+
+def print_world(msg):
+    print("{} world".format(msg))
+
 def print_hello():
     print("Hello")
-
-def print_goodbye():
-    print("Goodbye")
-
-var = ["AA", "BB", print_hello, print_goodbye]
-var[2]()
-var[3]()
