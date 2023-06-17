24dca03d0cfd292bdb150f7f507b6f3d6b980e62
Liberbell <liberbell@gmail.com>
2023-06-17 14:14:18 +0900

    base126

--- def/base12.py
+++ def/base12.py
@@ -7,6 +7,13 @@ def my_decorator(func):
     return wrapper
 
 @my_decorator
+def func_a(*args, **kwargs):
+    print("execute func_a")
+    print(args)
+
+@my_decorator
 def func_b(*args, **kwargs):
     print("execute func_b")
     print(args)
+
+func_a(1, 2, 3)
