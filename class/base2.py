07f02387b1e0c987c9a3685ee4b51fee146b193a
Liberbell <liberbell@gmail.com>
2023-06-18 15:08:15 +0900

    base24

--- class/base2.py
+++ class/base2.py
@@ -8,3 +8,6 @@ class SampleA():
         print("class val = {}".format(self.class_val))
         print("instance val = {}".format(self.instance_val))
 
+isinstance_a = SampleA()
+isinstance_a.set_val()
+print(isinstance_a.instance_val)
