f2cf348093e596c6abbb59b69f6cc054eb274682
Liberbell <liberbell@gmail.com>
2023-06-18 14:45:25 +0900

    base131

--- def/base13.py
+++ def/base13.py
@@ -4,3 +4,13 @@ map_a = map(lambda x: x * 2, list_a)
 
 for x in map_a:
     print(x)
+
+man = {
+    "name": "Bob",
+    "age": "65",
+    "country": "Jamaica",
+}
+
+map_man = map(lambda x: x + "," + man.get(x), man)
+for x in map_man:
+    print(x)
