c10b6cbb8d9a9c3f7cd1b277ef25be97cc8fbc0b
Liberbell <liberbell@gmail.com>
2023-06-20 08:39:14 +0900

    base54

--- class/base5.py
+++ class/base5.py
@@ -8,7 +8,7 @@ class Human:
         self.phone_number = phone_number
 
     def __str__(self):
-        return self.name + "," + str(self.age) + self.phone_number
+        return self.name + "," + str(self.age) + "," + self.phone_number
     
 man = Human("Alex", 32, "111-222-3333")
 man_str = str(man)
