6b8e70083e3daafb5a57163ed9376d1144e90344
Liberbell <liberbell@gmail.com>
2023-06-20 08:25:58 +0900

    base42

--- class/base4.py
+++ class/base4.py
@@ -9,3 +9,9 @@ class Human:
     def print_name_age(self):
         print("execute instance method")
         print("name: {}, age: {}".format(self.name, self.age))
+
+    @classmethod
+    def print_class_name(cls, msg):
+        print("execute class method")
+        print(cls.class_name)
+        print(msg)
