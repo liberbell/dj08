e050f4a72d7612325ee9c86bcd11c38f702133a5
Liberbell <liberbell@gmail.com>
2023-06-20 08:35:30 +0900

    base51

---
+++ class/base5.py
@@ -0,0 +1,11 @@
+
+
+class Human:
+    
+    def __init__(self, name ,age, phone_number):
+        self.name = name
+        self.age = age
+        self.phone_number = phone_number
+
+    def __str__(self):
+        return "human"
