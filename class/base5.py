46fced161484f0cfdadef4d1e7da4c5c7e17b00a
Liberbell <liberbell@gmail.com>
2023-06-20 08:45:13 +0900

    base57

--- class/base5.py
+++ class/base5.py
@@ -13,10 +13,13 @@ class Human:
     def __eq__(self, other):
         return (self.name == other.name) and (self.phone_number == other.phone_number)
     
+    def __hash__(self) -> int:
+        return hash(self.name + self.phone_number)
+    
 
     
 man = Human("Alex", 32, "111-222-3333")
-man2 = Human("Alex", 18, "111-222-4444")
+man2 = Human("Alex", 18, "111-222-3333")
 man_str = str(man)
 print(man_str)
 print(man == man2)
