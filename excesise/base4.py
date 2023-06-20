c22a42d354a2e493ff3f5a063c0690ac14d3ff4a
Liberbell <liberbell@gmail.com>
2023-06-21 08:46:54 +0900

    base42

--- excesise/base4.py
+++ excesise/base4.py
@@ -5,4 +5,14 @@ class AllCharacters:
     alive_characters = []
     dead_characters = []
 
+    @classmethod
+    def character_append(cls, name):
+        cls.all_characters.append(name)
+        cls.alive_characters.append(name)
+
+    @classmethod
+    def character_remove(cls, name):
+        cls.dead_characters.append(name)
+        cls.alive_characters.remove(name)
+
 class Character:
