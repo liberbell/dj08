6df3a3cfd293d6887d9b37750976a7345f0b27e2
Liberbell <liberbell@gmail.com>
2023-06-23 08:38:48 +0900

    base73

--- class/base7.py
+++ class/base7.py
@@ -6,3 +6,17 @@ class ClassA:
     def print_a(self):
         print("execute ClassA method")
         print("a = {}".format(self.a_name))
+
+    def print(self):
+        print("A hi")
+
+class ClassB:
+    def __init__(self, name):
+        self.b_name = name
+
+    def print_b(self):
+        print("execute ClassB method")
+        print("b = {}".format(self.b_name))
+
+    def print(self):
+        print("B hi")
