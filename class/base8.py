a078bb52046d2d80257471cf3298cae2d9c9b4c8
Liberbell <liberbell@gmail.com>
2023-06-24 15:22:37 +0900

    base82

--- class/base8.py
+++ class/base8.py
@@ -4,3 +4,7 @@ class Meta1(type):
     def __new__(metacls, name, bases, class_dict):
         print("metacls = {}".format(metacls))
         print("name = {}".format(name))
+        print("bases = {}".format(bases))
+        print("class_dict = {}".format(class_dict))
+
+        return super().__new__(metacls, name, bases, class_dict)
