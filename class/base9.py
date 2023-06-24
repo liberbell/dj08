4623c3202347864aa7742f50cdf54363210aa950
Liberbell <liberbell@gmail.com>
2023-06-24 15:43:35 +0900

    base92

--- class/base9.py
+++ class/base9.py
@@ -5,3 +5,10 @@ class Human(metaclass=ABCmeta):
 
     def __init__(self, name):
         self.name = name
+
+    @abstractmethod
+    def say_somthing(self):
+        pass
+
+    def say_name(self):
+        print(self.name)
