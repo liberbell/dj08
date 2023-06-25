3ee3ec40d49938c148a8e73dd8b77fef9f0111e1
Liberbell <liberbell@gmail.com>
2023-06-25 22:09:42 +0900

    base112

--- class/base11.py
+++ class/base11.py
@@ -5,3 +5,11 @@ class Human:
     def __init__(self, name, age):
         self.__name = name
         self.__age = age
+
+    def get_name(self):
+        print("call getter name")
+        return self.__name
+
+    def get_age(self):
+        print("call getter age")
+        return self.__age
