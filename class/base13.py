ce76a12eadacd8284ef20553d172a65eec9807da
Liberbell <liberbell@gmail.com>
2023-06-27 15:10:35 +0900

    base133

--- class/base13.py
+++ class/base13.py
@@ -2,5 +2,10 @@
 file_path = "../resources/input.csv"
 f = open(file_path, mode="r", encoding="utf-8")
 
-line = f.read()
-print(line)
+# line = f.read()
+# print(line)
+
+lines = f.readlines()
+print(lines)
+for x in lines:
+    print(x.rstrip("\n"))
