6a889a37b74bde3758b5bf024ba3dbbaa87e1107
Liberbell <liberbell@gmail.com>
2023-06-27 15:11:33 +0900

    base134

--- class/base13.py
+++ class/base13.py
@@ -5,7 +5,12 @@ f = open(file_path, mode="r", encoding="utf-8")
 # line = f.read()
 # print(line)
 
-lines = f.readlines()
-print(lines)
-for x in lines:
-    print(x.rstrip("\n"))
+# lines = f.readlines()
+# print(lines)
+# for x in lines:
+#     print(x.rstrip("\n"))
+
+line = f.readline()
+while line:
+    print(line.rstrip("\n"))
+    line = f.readline()
