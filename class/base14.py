21401feee08e8ec82688cc3e8289329c967e0803
Liberbell <liberbell@gmail.com>
2023-06-28 23:52:54 +0900

    base142

--- class/base14.py
+++ class/base14.py
@@ -4,7 +4,7 @@ file_path = "../resources/output.csv"
 f = open(file_path, "w", encoding="utf-8", newline="\n")
 f.write("aaa\nbbb")
 
-with open(file_path, mode="w", encoding="utf-8", newline="\n")as f:
+with open(file_path, mode="a", encoding="utf-8", newline="\n")as f:
     list_a = [
         ["A", "B", "C"],
         ["a", "b", "c"]
