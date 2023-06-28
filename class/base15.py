9c2a1f617c11a930fe7a8e4a152373ee9e98b64b
Liberbell <liberbell@gmail.com>
2023-06-28 23:58:59 +0900

    base155

--- class/base15.py
+++ class/base15.py
@@ -1,11 +1,13 @@
 
 class WithTest:
 
-    def __init__(self):
+    def __init__(self, msg):
         print("init called")
+        self.msg = msg
 
     def __enter__(self):
         print("entr called")
+        print(self.msg)
 
     def __exit__(self, exc_type, exc_val, traceback):
         print("exit called")
