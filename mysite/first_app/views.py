960bc412647b67dba1403950749332787e0b56aa
Liberbell <liberbell@gmail.com>
2023-07-04 08:38:08 +0900

    views4

--- mysite/first_app/views.py
+++ mysite/first_app/views.py
@@ -7,3 +7,6 @@ def index(request):
 
 def user_page(request, user_name):
     return HttpResponse(f"<h1>{user_name}\'s page</h1>")
+
+def number_page(request, number):
+    return HttpResponse(f"<h1>page number={number}</h1>")
