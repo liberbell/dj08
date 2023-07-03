f91a1c20f4bed6b45463fd45308ea0698b6eded0
Liberbell <liberbell@gmail.com>
2023-07-04 08:39:16 +0900

    urls5

--- mysite/first_app/urls.py
+++ mysite/first_app/urls.py
@@ -6,4 +6,5 @@ app_name = "first_app"
 urlpatterns = [
     path("hello", views.index, name="index"),
     path("page/<str:user_name>", views.user_page, name="user_page"),
+    path("number/<int:number>", views.number_page, name="number"),
 ]
