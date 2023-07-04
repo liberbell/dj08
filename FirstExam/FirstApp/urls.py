e02a27612e2c5d23e44f9d94ffbd33afd94ecdfd
Liberbell <liberbell@gmail.com>
2023-07-05 08:24:02 +0900

    urls3

--- FirstExam/FirstApp/urls.py
+++ FirstExam/FirstApp/urls.py
@@ -6,4 +6,5 @@ app_name = "first"
 urlpatterns = [
     path("add/<int:num1>/<int:num2>", views.add, name="add"),
     path("minus/<str:num1>/<str:num2>", views.minus, name="minus"),
+    path("div/<str:num1>/<str:num2>", views.div, name="div"),
 ]
