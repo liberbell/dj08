7cdfced17604eb88467dce6d27c2d200854db098
Liberbell <liberbell@gmail.com>
2023-07-05 08:20:41 +0900

    views2

--- FirstExam/FirstApp/views.py
+++ FirstExam/FirstApp/views.py
@@ -7,3 +7,8 @@ def add(request, num1, num2):
     return HttpResponse(f"<h1>{num1 + num2}</h1>")
 
 def minus(request, num1, num2):
+    num1 = float(num1)
+    num2 = float(num2)
+    return HttpResponse(f"<h1>{num1 - num2}</h1>")
+
+def div(request, num1, num2):
