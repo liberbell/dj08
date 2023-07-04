60ac3213ead5fb8f58b530ef51ad5307b2adf078
Liberbell <liberbell@gmail.com>
2023-07-05 08:19:36 +0900

    views1

--- FirstExam/FirstApp/views.py
+++ FirstExam/FirstApp/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def add(request, num1, num2):
-    return HttpResponse(f"<h1>")
+    return HttpResponse(f"<h1>{num1 + num2}</h1>")
+
+def minus(request, num1, num2):
