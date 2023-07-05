bf1156218e9ebe652daf63ead5bda73c8fd0da1f
Liberbell <liberbell@gmail.com>
2023-07-06 08:47:53 +0900

    views6

--- TemplateSample/TemplateProject/TemplateApp/views.py
+++ TemplateSample/TemplateProject/TemplateApp/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 def index(request):
     val = "Good bye!"
     return render(request, "TemplateApp/index.html", context={"value": val})
+
+def home(request):
+    my_name = "Eric"
+    favorite_fruits = ["apple", "lemon", "grape"]
+    my_info = {
+        "name": "Eric",
+        "age":  70
+    }
+    return render(request, "home.html", context={
+        "my_name": my_name,
+        "favorite_fruits": favorite_fruits,
+        "my_info": my_info
+    })
