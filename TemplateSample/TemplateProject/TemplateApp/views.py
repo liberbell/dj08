1df1f3420c79c47fdb2555bcc6c6077f0e02daf4
Liberbell <liberbell@gmail.com>
2023-07-06 08:41:54 +0900

    views5

--- TemplateSample/TemplateProject/TemplateApp/views.py
+++ TemplateSample/TemplateProject/TemplateApp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "TemplateApp/index.html", context={"value": "Hello!"})
+    val = "Good bye!"
+    return render(request, "TemplateApp/index.html", context={"value": val})
