5d68e22e49db469e23059bcb87397a9f7996d74e
Liberbell <liberbell@gmail.com>
2023-07-06 08:22:13 +0900

    urls1

--- TemplateSample/TemplateProject/TemplateProject/urls.py
+++ TemplateSample/TemplateProject/TemplateProject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("template_app", include("TemplateApp.urls")),
+    path("template_app/", include("TemplateApp.urls")),
 ]
