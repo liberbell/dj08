9af28ef552510ca4eaceef621f120ea7d84502f2
Liberbell <liberbell@gmail.com>
2023-07-10 08:14:22 +0900

    event_tags1

--- TemplateSample/TemplateProject/TemplateApp/templatetags/event_tags.py
+++ TemplateSample/TemplateProject/TemplateApp/templatetags/event_tags.py
@@ -5,3 +5,11 @@ register = template.Library()
 def convert_status_to_string(status):
     if status == 10:
         return "Success!"
+    elif status == 20:
+        return "Error"
+    elif status == 30:
+        return "Pending"
+    elif status == 40:
+        return "Failed"
+    else:
+        return "Unknown"
