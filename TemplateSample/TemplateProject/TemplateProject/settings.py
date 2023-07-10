bc2216eb7d539acb0069bd4311bb0570205707bd
Liberbell <liberbell@gmail.com>
2023-07-11 08:33:49 +0900

    settings2

--- TemplateSample/TemplateProject/TemplateProject/settings.py
+++ TemplateSample/TemplateProject/TemplateProject/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = {
+STATICFILES_DIRS = [
     STATIC_DIR
-}
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
