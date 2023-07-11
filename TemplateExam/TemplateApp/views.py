65b79f243db058dd9c32c87f9f52e34c1a022c92
Liberbell <liberbell@gmail.com>
2023-07-12 08:35:53 +0900

    views3

--- TemplateExam/TemplateApp/views.py
+++ TemplateExam/TemplateApp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class Member:
+
+    def __init__(self, name, id, join_at, picture_path):
+        self.name = name
+        self.id = id
+        self.join_at = join_at
+        self.picture_path = picture_path
+
+
 def home(request):
     return render(request, "home.html")
 
