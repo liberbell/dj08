9829b355489ae0b68fe54aae90d81ef57ef61d5b
Liberbell <liberbell@gmail.com>
2023-07-12 08:40:41 +0900

    views4

--- TemplateExam/TemplateApp/views.py
+++ TemplateExam/TemplateApp/views.py
@@ -4,11 +4,18 @@ from django.shortcuts import render
 
 class Member:
 
-    def __init__(self, name, id, join_at, picture_path):
+    def __init__(self, id, name, join_at, picture_path):
         self.name = name
         self.id = id
         self.join_at = join_at
         self.picture_path = picture_path
+
+member_list = [
+    Member(0, "Eric", "2023/01/10", "image/eric.jpg"),
+    Member(1, "Bob", "2023/01/20", "image/bob.jpg"),
+    Member(2, "Alex", "2023/01/05", "image/alex.jpg"),
+    Member(3, "Elton", "2023/02/04", "image/elton.jpg")
+]
 
 
 def home(request):
