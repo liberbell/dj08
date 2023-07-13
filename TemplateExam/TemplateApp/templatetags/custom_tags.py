1e639d8850830c93706ed544d717f0a6728c8fd7
Liberbell <liberbell@gmail.com>
2023-07-14 08:50:40 +0900

    custom_tags6

--- TemplateExam/TemplateApp/templatetags/custom_tags.py
+++ TemplateExam/TemplateApp/templatetags/custom_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from datetime import datetime
+import math
 
 register = template.Library()
 
@@ -8,3 +9,4 @@ def calculate_datetime_to_now(value):
     joined_datetime = datetime.strptime(value, "%Y/%m/%d")
     now_datetime = datetime.now()
     diff_datetime = now_datetime - joined_datetime
+    diff_days = diff_datetime.days
