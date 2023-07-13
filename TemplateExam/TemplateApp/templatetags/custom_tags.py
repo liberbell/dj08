a4e0b6dcc3ac47606333ef3b62301635e79588b9
Liberbell <liberbell@gmail.com>
2023-07-14 08:49:44 +0900

    custom_tags4

--- TemplateExam/TemplateApp/templatetags/custom_tags.py
+++ TemplateExam/TemplateApp/templatetags/custom_tags.py
@@ -6,3 +6,5 @@ register = template.Library()
 @register.filter(name="calculate_datetime_to_now")
 def calculate_datetime_to_now(value):
     joined_datetime = datetime.strptime(value, "%Y/%m/%d")
+    now_datetime = datetime.now()
+    diff_datetime = now_datetime - joined_datetime
