c42d76db91f47eb848f61e5d0282c43f3336b8fd
Liberbell <liberbell@gmail.com>
2023-07-14 09:59:06 +0900

    custom_tags7

--- TemplateExam/TemplateApp/templatetags/custom_tags.py
+++ TemplateExam/TemplateApp/templatetags/custom_tags.py
@@ -12,3 +12,4 @@ def calculate_datetime_to_now(value):
     diff_days = diff_datetime.days
     diff_years = math.floor(diff_days / 365)
     diff_months = math.floor((diff_days - 365 * diff_years) / 30)
+    return f"{diff_years}years {diff_months}months {diff_days}dyas"
