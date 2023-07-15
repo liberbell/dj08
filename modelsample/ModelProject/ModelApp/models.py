fc467a015ae3fc14f253bd9e23752c469add6332
Liberbell <liberbell@gmail.com>
2023-07-15 12:00:59 +0900

    models3

--- modelsample/ModelProject/ModelApp/models.py
+++ modelsample/ModelProject/ModelApp/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    
+
+class Sales(models.Model):
+    fees = models.IntegerField()
