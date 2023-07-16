aef642b8a9861e4eb7a3d1359c32a0c1b906b756
Liberbell <liberbell@gmail.com>
2023-07-16 21:12:22 +0900

    models2

--- modelsample/ModelProject/ModelApp/models.py
+++ modelsample/ModelProject/ModelApp/models.py
@@ -7,3 +7,6 @@ class Person(models.Model):
     birthday = models.DateField(default='1900-01-01')
     email = models.EmailField(db_index=True)
     salary = models.FloatField(null=True)
+    momo = models.TextField()
+    web_site = models.URLField(null=True, blank=True)
+    create_at = models.DateTimeField()
