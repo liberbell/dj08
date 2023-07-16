7bd3e6251dff78fcd9b955b0166afc388ae03fdf
Liberbell <liberbell@gmail.com>
2023-07-16 21:26:45 +0900

    models4

--- modelsample/ModelProject/ModelApp/models.py
+++ modelsample/ModelProject/ModelApp/models.py
@@ -9,7 +9,7 @@ class BaseMeta(models.Model):
 
     class Meta:
         abstract = True
-        
+
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -19,3 +19,7 @@ class Person(models.Model):
     momo = models.TextField()
     web_site = models.URLField(null=True, blank=True)
     create_at = models.DateTimeField(default=timezone.datetime.now)
+
+    class Meta:
+        db_table = 'person'
+        index_together = [["first_name", "last_name"]]
