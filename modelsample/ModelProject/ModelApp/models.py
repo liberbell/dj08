acbd9f951cb82ffbf16f5561e2d9e32815b05d47
Liberbell <liberbell@gmail.com>
2023-07-17 15:55:26 +0900

    models1

--- modelsample/ModelProject/ModelApp/models.py
+++ modelsample/ModelProject/ModelApp/models.py
@@ -11,15 +11,14 @@ class BaseMeta(models.Model):
     class Meta:
         abstract = True
 
-class Person(models.Model):
+class Person(BaseMeta):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     birthday = models.DateField(default='1900-01-01')
     email = models.EmailField(db_index=True)
     salary = models.FloatField(null=True)
-    momo = models.TextField()
+    memo = models.TextField()
     web_site = models.URLField(null=True, blank=True)
-    create_at = models.DateTimeField(default=timezone.datetime.now)
 
     class Meta:
         db_table = 'person'
