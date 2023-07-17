8e6922a28010f43d5e89b035834867d9d4d00d5d
Liberbell <liberbell@gmail.com>
2023-07-17 15:36:54 +0900

    main3

--- modelsample/ModelProject/main.py
+++ modelsample/ModelProject/main.py
@@ -3,4 +3,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ModelProject.settings')
 
 from django import setup
 setup()
+from ModelApp.models import Person
 
+p = Person(
+    first_name = "Eric", last_name = "Clapton", birthday = "2000-01-01",
+    email = "eric@example.com", salary = "20000", memo = "This is a memo",
+    web_site = "https://www.google.com"
+)
