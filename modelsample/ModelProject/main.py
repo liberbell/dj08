85647e43a4a1f6fd915731b7c8b36f07de984532
Liberbell <liberbell@gmail.com>
2023-07-17 15:38:30 +0900

    main6

--- modelsample/ModelProject/main.py
+++ modelsample/ModelProject/main.py
@@ -7,7 +7,7 @@ from ModelApp.models import Person
 
 p = Person(
     first_name = "Eric", last_name = "Clapton", birthday = "2000-01-01",
-    email = "eric@example.com", salary = "20000", memo = "This is a memo",
+    email = "eric@example.com", salary = "20000", momo = "This is a memo",
     web_site = "https://www.google.com"
 )
 p.save()
