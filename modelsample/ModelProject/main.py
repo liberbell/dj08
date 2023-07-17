5ed4337817b83eed01f69ae1fdb1fede69577cab
Liberbell <liberbell@gmail.com>
2023-07-17 15:44:59 +0900

    main4

--- modelsample/ModelProject/main.py
+++ modelsample/ModelProject/main.py
@@ -10,4 +10,10 @@ p = Person(
     email = "eric@example.com", salary =None , momo = "This is a memo",
     web_site = "https://www.google.com"
 )
+
+p = Person(
+    first_name = "Eric", last_name = "Clapton", birthday = "2000-01-01",
+    email = "eric@example.com", salary =None , momo = "This is a memo",
+    web_site = ""
+)
 p.save()
