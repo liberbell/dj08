7fc0f11bd99b24f7c4614b09181db314c165b47f
Liberbell <liberbell@gmail.com>
2023-07-19 08:30:47 +0900

    select_sample1

---
+++ modelsample/ModelProject/select_sample.py
@@ -0,0 +1,11 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ModelProject.settings')
+
+from django import setup
+setup()
+from ModelApp.models import Person
+
+persons = Person.objects.all()
+
+for person in persons:
+    print(person)
