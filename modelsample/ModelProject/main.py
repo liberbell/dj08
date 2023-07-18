9f7c71e7b317bdf632e1c0be14c82116cccc16be
Liberbell <liberbell@gmail.com>
2023-07-18 13:52:21 +0900

    main1

--- modelsample/ModelProject/main.py
+++ modelsample/ModelProject/main.py
@@ -7,18 +7,18 @@ from ModelApp.models import Person
 
 p = Person(
     first_name = "Eric", last_name = "Clapton", birthday = "2000-01-01",
-    email = "eric@example.com", salary =None , momo = "This is a memo",
+    email = "eric@example.com", salary =20000 , memo = "This is a memo",
     web_site = "https://www.google.com"
 )
 
-p = Person(
-    first_name = "Eric", last_name = "Clapton", birthday = "2000-01-01",
-    email = "eric@example.com", salary =None , momo = "This is a memo",
-    web_site = ""
-)
-# p.save()
+# p = Person(
+#     first_name = "Eric", last_name = "Clapton", birthday = "2000-01-01",
+#     email = "eric@example.com", salary =None , memo = "This is a memo",
+#     web_site = ""
+# )
+p.save()
 
-Person.objects.create(
-    first_name = "Alex", last_name = "Hepp", email = "alex@example.com", salary = 1500, momo = "class method",
-    web_site = None
-)
+# Person.objects.create(
+#     first_name = "Alex", last_name = "Hepp", email = "alex@example.com", salary = 1500, memo = "class method",
+#     web_site = None
+# )
