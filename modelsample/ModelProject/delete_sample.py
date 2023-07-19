ca84c259847f0198ed5b74673c1284909ef6f530
Liberbell <liberbell@gmail.com>
2023-07-20 08:39:06 +0900

    delete_sample1

---
+++ modelsample/ModelProject/delete_sample.py
@@ -0,0 +1,9 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ModelProject.settings')
+
+from django import setup
+setup()
+from ModelApp.models import Person
+from django.utils import timezone
+
+Person.objects.filter(first_name="alex").delete()
