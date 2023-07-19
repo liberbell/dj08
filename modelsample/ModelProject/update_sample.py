d4005d7186758f0ac2cbc5a8b4186fd4194b0b03
Liberbell <liberbell@gmail.com>
2023-07-20 08:28:15 +0900

    update_sample3

--- modelsample/ModelProject/update_sample.py
+++ modelsample/ModelProject/update_sample.py
@@ -10,5 +10,5 @@ import pytz
 person = Person.objects.get(id=1)
 print(person)
 person.birthday = "2022-01-01"
-update_at = timezone.datetime.now(pytz.timezone("Asia/Tokyo"))
+person.update_at = timezone.datetime.now(pytz.timezone("Asia/Tokyo"))
 person.save()
