18749b1025a410812fc765a26de83a634ad7d056
Liberbell <liberbell@gmail.com>
2023-07-21 08:42:48 +0900

    foreignkey_sample3

--- modelsample/ModelProject/foreignkey_sample.py
+++ modelsample/ModelProject/foreignkey_sample.py
@@ -8,3 +8,9 @@ from ModelApp.models import Students, Schools, Prefectures
 prefectures = ["Alaska", "Idaho"]
 schools = ["West high school", "East high school", "South high school", "North high school"]
 students = ["Eric", "Alex", "John"]
+
+def insert_record():
+    for prefecture_name in prefectures:
+        prefecture = Prefectures(
+            name = prefecture_name
+        )
