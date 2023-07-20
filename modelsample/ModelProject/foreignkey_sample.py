75f857c5c89836885db8e663810a4190e90e8d64
Liberbell <liberbell@gmail.com>
2023-07-21 08:40:57 +0900

    foreignkey_sample2

--- modelsample/ModelProject/foreignkey_sample.py
+++ modelsample/ModelProject/foreignkey_sample.py
@@ -3,3 +3,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ModelProject.settings')
 
 from django import setup
 setup()
+from ModelApp.models import Students, Schools, Prefectures
+
+prefectures = ["Alaska", "Idaho"]
+schools = ["West high school", "East high school", "South high school", "North high school"]
+students = ["Eric", "Alex", "John"]
