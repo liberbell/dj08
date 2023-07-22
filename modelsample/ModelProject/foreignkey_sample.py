389e0b7856d77acc693bf84f67ba2b27546705b2
Liberbell <liberbell@gmail.com>
2023-07-22 10:25:39 +0900

    foreignkey_sample8

--- modelsample/ModelProject/foreignkey_sample.py
+++ modelsample/ModelProject/foreignkey_sample.py
@@ -39,4 +39,6 @@ def select_students():
     for student in students:
         print(student.id, student.name, student.school.id, student.school.name, student.school.prefecture.id, student.school.prefecture.name)
 
-select_students()
+# select_students()
+
+Schools.objects.filter(id=1).delete()
