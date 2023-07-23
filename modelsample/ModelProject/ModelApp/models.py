3238e2deec65cd826bf06caf0d381b6db924c8a9
Liberbell <liberbell@gmail.com>
2023-07-23 16:01:54 +0900

    models6

--- modelsample/ModelProject/ModelApp/models.py
+++ modelsample/ModelProject/ModelApp/models.py
@@ -34,6 +34,7 @@ class Students(models.Model):
     age = models.IntegerField()
     major = models.CharField(max_length=20)
     school = models.ForeignKey("Schools", on_delete=models.RESTRICT)
+    prefecture = models.ForeignKey("Prefectures", on_delete=CASCADE)
 
     class Meta:
         db_table = "students"
