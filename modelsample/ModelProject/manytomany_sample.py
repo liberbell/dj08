b10453f38e1463a5fa1ad55db6c6f2911cbaf7c1
Liberbell <liberbell@gmail.com>
2023-07-25 08:40:12 +0900

    manytomany_sample3

--- modelsample/ModelProject/manytomany_sample.py
+++ modelsample/ModelProject/manytomany_sample.py
@@ -8,3 +8,10 @@ from ModelApp.models import Books, Authors
 def insert_books():
     book1 = Books(name="now and then")
     book2 = Books(name="still danger")
+    book3 = Books(name="yesterday")
+
+    book1.save()
+    book2.save()
+    book3.save()
+
+def insert_authors():
