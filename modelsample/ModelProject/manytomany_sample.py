9c0e5e746e71839fef7549b49a4204a5faadb3d5
Liberbell <liberbell@gmail.com>
2023-07-25 08:39:12 +0900

    manytomany_sample2

--- modelsample/ModelProject/manytomany_sample.py
+++ modelsample/ModelProject/manytomany_sample.py
@@ -3,3 +3,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ModelProject.settings')
 
 from django import setup
 setup()
+from ModelApp.models import Books, Authors
+
+def insert_books():
+    book1 = Books(name="now and then")
+    book2 = Books(name="still danger")
