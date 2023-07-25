67eeed1c8c7ab30c614bd6d5446a5eab4f1fe6f4
Liberbell <liberbell@gmail.com>
2023-07-26 08:43:32 +0900

    selectforeigntable5

--- modelsample/ModelProject/selectforeigntable.py
+++ modelsample/ModelProject/selectforeigntable.py
@@ -8,3 +8,7 @@ from ModelApp.models import Students, Schools, Prefectures
 s = Schools.objects.first()
 print(s)
 print(dir(s))
+print(s.prefecture.name)
+st = s.students_set
+print(type(st))
+print(dir(st))
