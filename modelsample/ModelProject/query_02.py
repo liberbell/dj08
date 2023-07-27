26fe6aa6e4819b72b12bafd5ca27b6137252b6ac
Liberbell <liberbell@gmail.com>
2023-07-28 08:44:20 +0900

    query_029

--- modelsample/ModelProject/query_02.py
+++ modelsample/ModelProject/query_02.py
@@ -20,5 +20,6 @@ ids = [13, 14, 15]
 # )
 # p.save()
 
-print(Person.objects.filter(salary__isnull=True).all())
-print(Person.objects.exclude(salary__isnull=True).all())
+# print(Person.objects.filter(salary__isnull=True).all())
+# print(Person.objects.exclude(salary__isnull=True).all())
+print(Students.objects.exclude(name="John").all())
