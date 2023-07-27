fb53eb2dcef141aec1b122d9956dd81a43442f49
Liberbell <liberbell@gmail.com>
2023-07-28 08:35:44 +0900

    query_021

---
+++ modelsample/ModelProject/query_02.py
@@ -0,0 +1,9 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ModelProject.settings')
+
+from django import setup
+setup()
+
+from ModelApp.models import Students
+
+print(Students.objects.all())
