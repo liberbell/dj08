f2593e64e93519b7983a56be09d2e0b9f46f57c9
Liberbell <liberbell@gmail.com>
2023-07-28 08:26:57 +0900

    query_012

--- modelsample/ModelProject/query_01.py
+++ modelsample/ModelProject/query_01.py
@@ -15,6 +15,10 @@ from ModelApp.models import Students
 # print(Students.objects.filter(age=17).all())
 
 # print(Students.objects.filter(name='John', pk=9).all())
-print(Students.objects.filter(name="John", pk__gt=13).all())
-print(Students.objects.filter(name="John", pk__lt=6).all())
-print(Students.objects.filter(name="John", pk__gt=6, pk__lt=20).all())
+# print(Students.objects.filter(name="John", pk__gt=13).all())
+# print(Students.objects.filter(name="John", pk__lt=6).all())
+# print(Students.objects.filter(name="John", pk__gt=6, pk__lt=20).all())
+
+print(Students.objects.all())
+print(Students.objects.filter(name__startswith="J").all())
+print(Students.objects.filter(name__endswith="x").all())
