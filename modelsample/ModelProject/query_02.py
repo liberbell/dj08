ee7ac9b18546aa6f2295e8e554e36b0f67776fca
Liberbell <liberbell@gmail.com>
2023-07-28 08:41:09 +0900

    django1

--- modelsample/ModelProject/query_02.py
+++ modelsample/ModelProject/query_02.py
@@ -4,11 +4,18 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ModelProject.settings')
 from django import setup
 setup()
 
-from ModelApp.models import Students
+from ModelApp.models import Students, Person
 
 # print(Students.objects.all())
 
 ids = [13, 14, 15]
 # print(Students.objects.filter(pk__in=ids).all())
 
-print(Students.objects.filter(name__contains="h").all())
+# print(Students.objects.filter(name__contains="h").all())
+
+p = Person(
+    first_name = "Eric", last_name = "Clapton", birthday = "2000-01-01",
+    email = "eric@example.com", salary =None , memo = "This is a memo",
+    web_site = "https://www.google.com"
+)
+p.save()
