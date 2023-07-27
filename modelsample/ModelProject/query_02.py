8aead85fade0c5f6915f49d3d93005018251a5ae
Liberbell <liberbell@gmail.com>
2023-07-28 08:43:00 +0900

    query_028

--- modelsample/ModelProject/query_02.py
+++ modelsample/ModelProject/query_02.py
@@ -13,9 +13,11 @@ ids = [13, 14, 15]
 
 # print(Students.objects.filter(name__contains="h").all())
 
-p = Person(
-    first_name = "Alex", last_name = "Hepp", birthday = "2000-01-01",
-    email = "alex@example.com", salary =100 , memo = "This is a memo",
-    web_site = "https://www.google.com"
-)
-p.save()
+# p = Person(
+#     first_name = "Alex", last_name = "Hepp", birthday = "2000-01-01",
+#     email = "alex@example.com", salary =100 , memo = "This is a memo",
+#     web_site = "https://www.google.com"
+# )
+# p.save()
+
+print(Person.objects.filter(salary__isnull=True).all())
