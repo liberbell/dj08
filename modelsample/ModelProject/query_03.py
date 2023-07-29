adde691b7b50688fee3622b3072a366e42c9bb54
Liberbell <liberbell@gmail.com>
2023-07-29 10:51:50 +0900

    query_035

--- modelsample/ModelProject/query_03.py
+++ modelsample/ModelProject/query_03.py
@@ -9,4 +9,6 @@ from ModelApp.models import Students
 # print(Students.objects.count())
 # print(Students.objects.filter(name="John").count())
 
-from django.db.models import Count, Max, Avg, Min
+from django.db.models import Count, Max, Avg, Min, Sum
+
+print(Students.objects.aaggregate(Count("pk"), Max("pk"), Min("pk"), Avg("pk"), Sum("pk")))
