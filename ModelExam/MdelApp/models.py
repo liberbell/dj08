86bf2d1fc3590df2c4c2c076ffb4dd82572053aa
Liberbell <liberbell@gmail.com>
2023-07-31 08:37:56 +0900

    models2

--- ModelExam/MdelApp/models.py
+++ ModelExam/MdelApp/models.py
@@ -10,7 +10,17 @@ class Classes(models.Model):
 class Students(models.Model):
     name = models.CharField(max_length=50)
     grade = models.IntegerField()
-    class_fk = models.ForeignKey(Classes, on_delete=models.CASCADE)
+    class_fk = models.ForeignKey("Classes", on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'students'
+
+class Tests(models.Model):
+    name = models.CharField(max_length=50)
+    
+    class Meta:
+        db_table = 'tests'
+
+class TestResults(models.Model):
+    score = models.IntegerField()
+    students = models.ForeignKey("Students", on_delete=models.CASCADE)
