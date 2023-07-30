be939795b860675cec3ace9333f8567ae1eff9f9
Liberbell <liberbell@gmail.com>
2023-07-31 08:46:48 +0900

    01_insert_record2

--- ModelExam/ModelExam/01_insert_record.py
+++ ModelExam/ModelExam/01_insert_record.py
@@ -1,1 +1,7 @@
 import os
+
+os.environ.setdefault("django_SETINGS_MODLE", "ModelExam.settings")
+from django import setup
+setup()
+
+from ModelApp.models import Students, TestResults, Tests, Classes
