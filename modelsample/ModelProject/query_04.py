3b01378e2c5dfaccda5ca77479e4f26444d5e635
Liberbell <liberbell@gmail.com>
2023-07-30 17:11:34 +0900

    query_044

--- modelsample/ModelProject/query_04.py
+++ modelsample/ModelProject/query_04.py
@@ -6,8 +6,11 @@ setup()
 
 from ModelApp.models import Students, Schools
 
-for student in Students.objects.all():
-    print(student.name, student.school.name, student.school.prefecture.name)
+# for student in Students.objects.all():
+#     print(student.name, student.school.name, student.school.prefecture.name)
 
-for student in Students.objects.filter(school__name="West high school").all():
+# for student in Students.objects.filter(school__name="West high school").all():
+#     print(student.name, student.school.name, student.school.prefecture.name)
+
+for student in Students.objects.exclude(school__name="West high school").all():
     print(student.name, student.school.name, student.school.prefecture.name)
