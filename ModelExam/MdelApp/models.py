ca98ea57f456b6b397233bde8200aeb7ef5365c6
Liberbell <liberbell@gmail.com>
2023-07-31 08:36:19 +0900

    models1

--- ModelExam/MdelApp/models.py
+++ ModelExam/MdelApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Classes(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'classes'
+
+class Students(models.Model):
+    name = models.CharField(max_length=50)
+    grade = models.IntegerField()
+    class_fk = models.ForeignKey(Classes, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'students'
