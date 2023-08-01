6135b90a78e676dda34a66dd285df12923e5e924
Liberbell <liberbell@gmail.com>
2023-08-01 10:10:12 +0900

    01_select_records5

--- ModelExam/ModelExam/01_select_records.py
+++ ModelExam/ModelExam/01_select_records.py
@@ -5,3 +5,7 @@ from django import setup
 setup()
 
 from ModelApp.models import Students
+
+student = Students.objects.get(pk=1)
+for test_result in student.testresults_set.all():
+    print(student.class_fk.name, student.name, test_result.test.name, test_result.score)
