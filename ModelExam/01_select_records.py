e2573689974955f33cc22205631aea72d17dca7f
Liberbell <liberbell@gmail.com>
2023-08-01 11:58:45 +0900

    01_select_records10

--- ModelExam/01_select_records.py
+++ ModelExam/01_select_records.py
@@ -22,5 +22,7 @@ for class_summary in Classes.objects.values("name", "students__testresults__test
     class_summary["name"],
     class_summary["students__testresults__test__name"],
     class_summary["max_score"],
-    
+    class_summary["min_score"],
+    class_summary["avg_score"],
+    class_summary["sum_score"],
     )
