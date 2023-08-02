7f6ed9e9d59ebd7f180031accdcf184f188e2f3d
Liberbell <liberbell@gmail.com>
2023-08-03 08:16:45 +0900

    views2

--- FormSample/FormApp/views.py
+++ FormSample/FormApp/views.py
@@ -3,4 +3,10 @@ from . import forms
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "formapp/index.html")
+
+def form_page(request):
+    form = forms.UserInfo()
+    return render(request, "formapp/form_page.html", context={
+        "form": form
+    })
