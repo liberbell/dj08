e4ec5d09b38196b8596c28e70b761dd3f8b6e61c
Liberbell <liberbell@gmail.com>
2023-08-03 08:18:57 +0900

    urls2

--- FormSample/FormApp/urls.py
+++ FormSample/FormApp/urls.py
@@ -2,3 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = "form_app"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("form_page/", views.form_page, name="form_page"),
+]
