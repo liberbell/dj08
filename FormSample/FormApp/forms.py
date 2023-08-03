225c7ccf012d717bad0d2fce7a0a27eed2dc35a0
Liberbell <liberbell@gmail.com>
2023-08-04 08:47:23 +0900

    forms1

--- FormSample/FormApp/forms.py
+++ FormSample/FormApp/forms.py
@@ -4,3 +4,18 @@ class UserInfo(forms.Form):
     name = forms.CharField()
     age = forms.IntegerField()
     mail = forms.EmailField()
+    is_married = forms.BooleanField()
+    birthday = forms.DateField()
+    salary = forms.IntegerField()
+    job = forms.ChoiceField(choices=(
+        (1, "Permanent"),
+        (2, "Temporary"),
+        (3, "Student"),
+        (4, "No job")
+    ))
+    hobby = forms.MultipleChoiceField(choices=(
+        (1, "Baseball"),
+        (2, "Books"),
+        (3, "Movies"),
+        (4, "Other")
+    ))
