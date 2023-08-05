1ab47943a19ac912bc9ef34c0e752d5d78118db4
Liberbell <liberbell@gmail.com>
2023-08-05 15:27:28 +0900

    forms5

--- FormSample/FormApp/forms.py
+++ FormSample/FormApp/forms.py
@@ -27,4 +27,5 @@ class UserInfo(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super(UserInfo, self).__init__(*args, **kwargs)
-        
+        self.fields["job"].widget.attrs["id"] = "id_job"
+        self.fields["hobby"].widget.attrs["class"] = "hobbies_class"
