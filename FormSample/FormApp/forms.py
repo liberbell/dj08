d8f85cd82a11cc088fc93955f4930d0839ec184a
Liberbell <liberbell@gmail.com>
2023-08-06 13:45:10 +0900

    forms5

--- FormSample/FormApp/forms.py
+++ FormSample/FormApp/forms.py
@@ -3,7 +3,7 @@ from django.core import validators
 
 class UserInfo(forms.Form):
     name = forms.CharField(label="Name", min_length=2, max_length=15)
-    age = forms.IntegerField(label="Age", validators=[validators.MinValueValidator()])
+    age = forms.IntegerField(label="Age", validators=[validators.MinValueValidator(20, message="Input over 20")])
     mail = forms.EmailField(
         label="E-mail",
         widget=forms.TextInput(attrs={"class": "mail_class", "placeholder": "sample@example.com"})
