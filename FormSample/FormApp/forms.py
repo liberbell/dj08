b440cd5c4e1ad41e3066243db7cb05b5f3edcf04
Liberbell <liberbell@gmail.com>
2023-08-08 08:21:58 +0900

    forms1

--- FormSample/FormApp/forms.py
+++ FormSample/FormApp/forms.py
@@ -55,4 +55,5 @@ class UserInfo(forms.Form):
 class PostModelForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ["name", "title"]
