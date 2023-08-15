2919a45f8d411e0c595ceff499f63c6ce68ece55
Liberbell <liberbell@gmail.com>
2023-08-16 08:38:32 +0900

    views1

--- FormExam/FormApp/views.py
+++ FormExam/FormApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
+def insert_student(request):
+    insert_form = forms.StudentInsertForm(request.POST or None, request.FILES or None)
+    if insert_form.is_valid():
