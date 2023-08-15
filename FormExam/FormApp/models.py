452bef17516c2fdf86e9c26bb30a4bf9e4371cf1
Liberbell <liberbell@gmail.com>
2023-08-16 08:31:05 +0900

    models1

--- FormExam/FormApp/models.py
+++ FormExam/FormApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    grade = models.IntegerField()
+    picture = models.FileField(upload_to='student/')
