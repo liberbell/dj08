2dcd73e867be8f0ad9f72516603400c204d77783
Liberbell <liberbell@gmail.com>
2023-08-16 08:39:55 +0900

    views3

--- FormExam/FormApp/views.py
+++ FormExam/FormApp/views.py
@@ -5,3 +5,7 @@ from . import forms
 def insert_student(request):
     insert_form = forms.StudentInsertForm(request.POST or None, request.FILES or None)
     if insert_form.is_valid():
+        insert_form.save()
+        insert_form = forms.StudentInsertForm()
+    return render(request, "formapp/insert_student.html",
+                  context={'insert_form': insert_form})
