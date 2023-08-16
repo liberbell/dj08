03e32643db1c4ef33e76db298291e25dd32c27a6
Liberbell <liberbell@gmail.com>
2023-08-17 08:45:32 +0900

    views1

--- FormExam/FormApp/views.py
+++ FormExam/FormApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import forms
+from .models import Students
 
 # Create your views here.
 def insert_student(request):
@@ -9,3 +10,10 @@ def insert_student(request):
         insert_form = forms.StudentInsertForm()
     return render(request, "formapp/insert_student.html",
                   context={'insert_form': insert_form})
+
+def students_list(request):
+    students = Students.objects.all()
+    return render(
+        request, "formapp/students_list.html",
+        context={"students": students},
+    )
