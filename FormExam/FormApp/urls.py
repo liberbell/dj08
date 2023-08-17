97ba5db9158ed5e33a534d5b30954da0246eace0
Liberbell <liberbell@gmail.com>
2023-08-18 08:39:31 +0900

    urls3

--- FormExam/FormApp/urls.py
+++ FormExam/FormApp/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "form_app"
 
 urlpatterns = [
-    path("insert_student", views.insert_student, name="insert_student"),
+    path("insert_student/", views.insert_student, name="insert_student"),
     path("students_list/", views.students_list, name="students_list"),
 ]
