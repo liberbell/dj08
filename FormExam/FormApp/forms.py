7e83ded316625a96f21d54c3302fac6c1044a951
Liberbell <liberbell@gmail.com>
2023-08-18 08:40:16 +0900

    forms1

--- FormExam/FormApp/forms.py
+++ FormExam/FormApp/forms.py
@@ -11,3 +11,13 @@ class StudentInsertForm(forms.ModelForm):
     class Meta:
         model = Students
         fields = "__all__"
+
+class StudentEditForm(forms.Form):
+    name = forms.CharField(label="Name: ")
+    age = forms.IntegerField(label="Age: ")
+    grade = forms.IntegerField(label="Grade: ")
+    picture = forms.FileField(label="Picture: ")
+
+    class Meta:
+        model = Students
+        fields = "__all__"
