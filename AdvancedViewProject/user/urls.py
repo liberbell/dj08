974f0fe4c78cb1f84b5bdc07f20c4af1ec0e879c
Liberbell <liberbell@gmail.com>
2023-08-28 08:35:11 +0900

    urls2

--- AdvancedViewProject/user/urls.py
+++ AdvancedViewProject/user/urls.py
@@ -6,4 +6,5 @@ app_name = "user"
 urlpatterns = [
     path("user_list/", views.user_list, name="user_list",),
     path("index/", views.index, name="index",),
+    path("register/", views.register, name="register",)
 ]
