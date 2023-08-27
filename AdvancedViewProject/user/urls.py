2392a0e1412c3afc850638fec328f0f2134f66eb
Liberbell <liberbell@gmail.com>
2023-08-28 08:34:10 +0900

    urls1

--- AdvancedViewProject/user/urls.py
+++ AdvancedViewProject/user/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "user"
 
 urlpatterns = [
-    path("user_list/", views.user_list, name="user_list",)
+    path("user_list/", views.user_list, name="user_list",),
+    path("index/", views.index, name="index",),
 ]
