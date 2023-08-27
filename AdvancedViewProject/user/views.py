8aa20665ca3eff7a489131d80c96ac665696801f
Liberbell <liberbell@gmail.com>
2023-08-27 14:49:09 +0900

    views1

--- AdvancedViewProject/user/views.py
+++ AdvancedViewProject/user/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def user_list(request):
     return render(request, "user/user_list.html")
+
+def index(request):
+    return render(request, "user/index.html")
