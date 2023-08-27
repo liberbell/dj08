a8be96b5331e8512afb130b9d468f7bdf293acbb
Liberbell <liberbell@gmail.com>
2023-08-27 14:42:04 +0900

    models3

--- AdvancedViewProject/user/models.py
+++ AdvancedViewProject/user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class profile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete="models.CASCADE")
     website = models.URLField(blank=True)
     picture = models.FileField(upload_to="user/", blank=True)
