8955bffcc92f4c93ca5b11f3d9eacec5191b0531
Liberbell <liberbell@gmail.com>
2023-08-27 14:45:36 +0900

    forms3

--- AdvancedViewProject/user/forms.py
+++ AdvancedViewProject/user/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from user.models import Profile
 
 class Userform(forms.ModelForm):
+    username = forms.CharField(label="Username")
+    email = forms.EmailField(label="Email")
     password = forms.CharField(label="Password", widget=forms.PasswordInput())
 
     class Meta:
