3c345e29360b612463c17ca4bff6d6550a1f59fe
Liberbell <liberbell@gmail.com>
2023-09-01 08:54:12 +0900

    models2

--- CustomUser/accounts/models.py
+++ CustomUser/accounts/models.py
@@ -2,3 +2,8 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
