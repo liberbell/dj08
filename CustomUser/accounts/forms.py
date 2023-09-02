ecb4ae5b23c8f7f510ecd613815a7ae241af4ad0
Liberbell <liberbell@gmail.com>
2023-09-02 15:10:34 +0900

    forms2

--- CustomUser/accounts/forms.py
+++ CustomUser/accounts/forms.py
@@ -1,2 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+user = get_user_model()
