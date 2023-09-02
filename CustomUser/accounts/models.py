cd2da1b120ea233b9667e0e7d298235a0555c318
Liberbell <liberbell@gmail.com>
2023-09-02 14:53:22 +0900

    models1

--- CustomUser/accounts/models.py
+++ CustomUser/accounts/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        
+
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=150)
     email = models.EmailField(max_length=255, unique=True)
@@ -12,3 +17,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
