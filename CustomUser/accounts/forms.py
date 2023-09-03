cb6d10a4864948a35596f99eb8f60032db975c0c
Liberbell <liberbell@gmail.com>
2023-09-03 12:12:06 +0900

    forms1

--- CustomUser/accounts/forms.py
+++ CustomUser/accounts/forms.py
@@ -7,3 +7,4 @@ user = get_user_model()
 
 class UserCreationForm(forms.ModelForm):
     password = forms.CharField(label="Password:", widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label="Reinpu password:", widget=forms.PasswordInput)
