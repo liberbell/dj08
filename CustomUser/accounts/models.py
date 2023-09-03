f6bdb889e26f335619f95496bde3c56a8fa8e7f2
Liberbell <liberbell@gmail.com>
2023-09-04 08:44:35 +0900

    models2

--- CustomUser/accounts/models.py
+++ CustomUser/accounts/models.py
@@ -50,3 +50,12 @@ class Students(models.Model):
     school = models.ForeignKey(
         'schools', on_delete=models.CASCADE
     )
+
+    class Meta:
+        db_table = 'students'
+
+class Schools(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'schools'
