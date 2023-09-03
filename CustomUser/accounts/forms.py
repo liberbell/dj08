d10a84a194401278b2da7bbcba723844927f9b95
Liberbell <liberbell@gmail.com>
2023-09-03 12:13:41 +0900

    forms2

--- CustomUser/accounts/forms.py
+++ CustomUser/accounts/forms.py
@@ -2,9 +2,14 @@ from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from .models import User
 
 user = get_user_model()
 
 class UserCreationForm(forms.ModelForm):
     password = forms.CharField(label="Password:", widget=forms.PasswordInput)
     confirm_password = forms.CharField(label="Reinpu password:", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password")
