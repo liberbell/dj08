92138d027a6d6e9d4239d410e675fb9f48e48757
Liberbell <liberbell@gmail.com>
2023-09-07 08:35:27 +0900

    models1

--- FunctionExam/accounts/models.py
+++ FunctionExam/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
+class Users(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255)
+    age = models.PositiveIntegerField()
+    email = models.EmailField(max_length=25, unique=True)
+    is_active = models.BooleanField(default=False)
