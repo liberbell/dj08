ed57360b62895638a2b9e53c59cf329a4ac07d81
Liberbell <liberbell@gmail.com>
2023-09-07 08:36:25 +0900

    models2

--- FunctionExam/accounts/models.py
+++ FunctionExam/accounts/models.py
@@ -7,3 +7,5 @@ class Users(AbstractBaseUser, PermissionsMixin):
     age = models.PositiveIntegerField()
     email = models.EmailField(max_length=25, unique=True)
     is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    picture = models.FileField(null=True, upload_to="picture/")
