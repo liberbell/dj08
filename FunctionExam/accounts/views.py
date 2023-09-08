051407aa74da19785d94d1d0e56e4c71d9b36780
Liberbell <liberbell@gmail.com>
2023-09-08 23:27:43 +0900

    views1

--- FunctionExam/accounts/views.py
+++ FunctionExam/accounts/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "accounts/home.html")
+
+def regist(request):
+    regist_fomr = forms.RegistForm(request.POST)
