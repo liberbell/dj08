4a7f905d34aa81cda89b3dd345df8b6fea564af5
Liberbell <liberbell@gmail.com>
2023-09-08 23:31:33 +0900

    forms3

--- FunctionExam/accounts/forms.py
+++ FunctionExam/accounts/forms.py
@@ -4,4 +4,6 @@ from .models import Users
 
 class RegistForm(forms.ModelForm):
     username = forms.CharField(label="Name:")
-    age = forms.IntegerField(label="Age:")
+    age = forms.IntegerField(label="Age:", min_value=0)
+    email = forms.EmailField(label="Email:")
+    password = forms.CharField(label="Password:", widget=forms.PasswordInput())
