18289087b3da76cb750e950d401474325fe565ea
Liberbell <liberbell@gmail.com>
2023-09-09 17:35:50 +0900

    forms1

--- FunctionExam/accounts/forms.py
+++ FunctionExam/accounts/forms.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict
 from django import forms
 from .models import Users
+from django.contrib.auth.password_validation import validate_password
 
 
 class RegistForm(forms.ModelForm):
@@ -20,3 +21,7 @@ class RegistForm(forms.ModelForm):
         confirm_password = cleaned_data["confirm_password"]
         if password != confirm_password:
             raise forms.ValidationError("Invalid password")
+        
+    def save(self, commit=False):
+        user = super().save(commit=False)
+        validate_password(self.cleaned_data["password"], user)
