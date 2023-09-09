6c8ee3e673b3f83bd2985b5b7b9fd363d3681c04
Liberbell <liberbell@gmail.com>
2023-09-09 17:36:39 +0900

    forms2

--- FunctionExam/accounts/forms.py
+++ FunctionExam/accounts/forms.py
@@ -25,3 +25,6 @@ class RegistForm(forms.ModelForm):
     def save(self, commit=False):
         user = super().save(commit=False)
         validate_password(self.cleaned_data["password"], user)
+        user.set_password(self.cleaned_data["password"])
+        user.save()
+        return user
