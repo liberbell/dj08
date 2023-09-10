04146fc3ed59dea99f1cbd536daf0b05a43ed749
Liberbell <liberbell@gmail.com>
2023-09-10 12:17:26 +0900

    urls1

--- FunctionExam/accounts/urls.py
+++ FunctionExam/accounts/urls.py
@@ -6,4 +6,5 @@ app_name = "accounts"
 urlpatterns = [
     path("", views.home, name="home"),
     path("regist/", views.regist, name="regist"),
+    path("activate/<uuid:token>", views.activate_user, name="activate_user"),
 ]
