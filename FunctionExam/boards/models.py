224742bbe6b2b7ba0ca31a4a567b62a03f0a9f48
Liberbell <liberbell@gmail.com>
2023-09-14 08:08:44 +0900

    models1

--- FunctionExam/boards/models.py
+++ FunctionExam/boards/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Theme(models.Model):
+
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        "accounts.User",
+        on_delete=models.CASCADE
+    )
