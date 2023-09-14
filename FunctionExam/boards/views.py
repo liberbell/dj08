785b099d327f109b70222b2c043243abc2712513
Liberbell <liberbell@gmail.com>
2023-09-15 08:18:56 +0900

    views2

--- FunctionExam/boards/views.py
+++ FunctionExam/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import forms
 
 # Create your views here.
@@ -6,7 +6,9 @@ def create_theme(request):
     create_theme_form = forms.CreateThemeForm(request.POST or None)
 
     if create_theme_form.is_valid():
+        create_theme_form.instance.user = request.user
         create_theme_form.save()
+        return redirect("accounts:home")
     return render(request, "boards/create_theme.html",
                   context={
                       "create_theme_form": create_theme_form
