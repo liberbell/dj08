d6ab33634b9a9f0253bf852aed61f8e9ec986e3d
Liberbell <liberbell@gmail.com>
2023-09-15 08:11:55 +0900

    views2

--- FunctionExam/boards/views.py
+++ FunctionExam/boards/views.py
@@ -3,4 +3,11 @@ from . import forms
 
 # Create your views here.
 def create_theme(request):
-    create_theme_form = forms.CreateThemesForm(request.POST or None)
+    create_theme_form = forms.CreateThemeForm(request.POST or None)
+
+    if create_theme_form.is_valid():
+        create_theme_form.save()
+    return render(request, "boards/create_theme.html",
+                  context={
+                      "create_theme_form": create_theme_form
+                  })
