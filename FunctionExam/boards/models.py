876cec054d6d05297fd2e3b7314217420b926bef
Liberbell <liberbell@gmail.com>
2023-09-15 08:32:09 +0900

    models1

--- FunctionExam/boards/models.py
+++ FunctionExam/boards/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ThemesManager(models.Manager):
+
+    def fetch_all_theme(self):
+        return self.order_by("id").all()
+
 class Themes(models.Model):
 
     title = models.CharField(max_length=255)
