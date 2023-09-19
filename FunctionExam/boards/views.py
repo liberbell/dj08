5e6cb53ac12ef94aa44fd6b75ec6387b6894f12b
Liberbell <liberbell@gmail.com>
2023-09-19 16:57:16 +0900

    views3

--- FunctionExam/boards/views.py
+++ FunctionExam/boards/views.py
@@ -57,7 +57,7 @@ def delete_theme(request, id):
 def post_comment(request, theme_id):
     post_comment_form = forms.PostCommentForm(request.POST or None)
     theme = get_object_or_404(Themes, id=theme_id)
-    comments = Comments.fetch_by_theme_id(theme.id)
+    comments = Comments.objects.fetch_by_theme_id(theme.id)
     if post_comment_form.is_valid():
         post_comment_form.instance.theme = theme
         post_comment_form.instance.user = request.user
