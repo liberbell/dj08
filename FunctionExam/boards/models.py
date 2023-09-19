1f3db5dfa6ab6ed445fc2ab362148368335b9182
Liberbell <liberbell@gmail.com>
2023-09-19 11:28:29 +0900

    models1

--- FunctionExam/boards/models.py
+++ FunctionExam/boards/models.py
@@ -18,6 +18,11 @@ class Themes(models.Model):
     class Meta:
         db_table = 'thems'
 
+class CommentsManager(models.Manager):
+
+    def fetch_by_theme_id(self, theme_id):
+        return self.filter(theme_id=theme_id).order_by("id").all()
+
 class Comments(models.Model):
 
     comment = models.CharField(max_length=1000)
