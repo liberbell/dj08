aaed22226e0ba256ca4e99873fbc0a2112fca51c
Liberbell <liberbell@gmail.com>
2023-09-21 08:50:20 +0900

    urls1

--- FunctionExam/boards/urls.py
+++ FunctionExam/boards/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("edit_theme/<int:id>", views.edit_theme, name="edit_theme"),
     path("delete_theme/<int:id>", views.delete_theme, name="delete_theme"),
     path("post_comment/<int:theme_id>", views.post_comment, name="post_comment"),
+    path("save_comment/", views.save_comment, name="save_comment"),
 ]
