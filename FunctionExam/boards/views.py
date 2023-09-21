33836306660a67b7b5622f8302f83318b3d0a6e0
Liberbell <liberbell@gmail.com>
2023-09-22 08:35:10 +0900

    views4

--- FunctionExam/boards/views.py
+++ FunctionExam/boards/views.py
@@ -59,7 +59,7 @@ def delete_theme(request, id):
 def post_comment(request, theme_id):
     saved_comment = cache.get(f'saved_comment-theme_id={theme_id}-user_id={request.user.id}', "")
     post_comment_form = forms.PostCommentForm(request.POST or None,
-                                              initial={"comment": save_comment})
+                                              initial={"comment": saved_comment})
     theme = get_object_or_404(Themes, id=theme_id)
     comments = Comments.objects.fetch_by_theme_id(theme.id)
     if post_comment_form.is_valid():
