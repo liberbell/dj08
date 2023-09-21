96490e78b3520b4602712f8b403c33aade1d0b17
Liberbell <liberbell@gmail.com>
2023-09-21 09:01:08 +0900

    views4

--- FunctionExam/boards/views.py
+++ FunctionExam/boards/views.py
@@ -75,7 +75,7 @@ def post_comment(request, theme_id):
                   })
 
 def save_comment(request):
-    if request.is_ajax():
+    if request.is_ajax:
         comment = request.GET.get("comment")
         theme_id = request.GET.get("theme_id")
         if comment and theme_id:
