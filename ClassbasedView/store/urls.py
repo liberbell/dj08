1125d6c0aa6883212dde5c34b3f49358f65ca771
Liberbell <liberbell@gmail.com>
2023-09-25 09:24:02 +0900

    urls3

--- ClassbasedView/store/urls.py
+++ ClassbasedView/store/urls.py
@@ -7,5 +7,5 @@ app_name = "store"
 urlpatterns = [
     path("index/", IndexView.as_view(), name="index"),
     # path("home/", TemplateView.as_view(template_name="home.html"), name="home"),
-    path("home/", HomeView.as_view(), name="home"),
+    path("home/<name>", HomeView.as_view(), name="home"),
 ]
