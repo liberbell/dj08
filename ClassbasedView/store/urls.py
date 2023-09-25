64eab061cc7c40594a3b2995896e3a1114877c2d
Liberbell <liberbell@gmail.com>
2023-09-25 09:14:50 +0900

    urls2

--- ClassbasedView/store/urls.py
+++ ClassbasedView/store/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, HomeView
 # from django.views.generic.base import TemplateView
+from 
 
 app_name = "store"
 
 urlpatterns = [
     path("index/", IndexView.as_view(), name="index"),
     # path("home/", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("home/", HomeView.as_view(), name="home"),
 ]
