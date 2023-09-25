dee56694ef5b348679afdbf8bdad5dc409b73bd8
Liberbell <liberbell@gmail.com>
2023-09-25 09:22:29 +0900

    views4

--- ClassbasedView/store/views.py
+++ ClassbasedView/store/views.py
@@ -29,3 +29,4 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["time"] = datetime.now()
+        return context
