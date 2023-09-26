6ec8faf692f3f7ceddd71b769127528013cfd5e4
Liberbell <liberbell@gmail.com>
2023-09-27 08:49:49 +0900

    urls3

--- ClassbasedView/store/urls.py
+++ ClassbasedView/store/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import IndexView, HomeView, BookDetailView, BookListView, BookCreateView
+from .views import (
+    IndexView, HomeView, BookDetailView, BookListView,
+    BookCreateView
+    )
 # from django.views.generic.base import TemplateView
 
 app_name = "store"
