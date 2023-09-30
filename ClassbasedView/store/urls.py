eb03c75045a9cc8af7b1ece3a7772e9481d0f894
Liberbell <liberbell@gmail.com>
2023-09-30 10:37:34 +0900

    urls2

--- ClassbasedView/store/urls.py
+++ ClassbasedView/store/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path("add_book/", BookCreateView.as_view(), name="add_book"),
     path("edit_book/<int:pk>", BookUpdateView.as_view(), name="edit_book"),
     path("delete_book/<int:pk>", BookDeleteView.as_view(), name="delete_book"),
-    path("book_form/>", BookFormView.as_view(), name="book_form"),
+    path("book_form/", BookFormView.as_view(), name="book_form"),
 ]
