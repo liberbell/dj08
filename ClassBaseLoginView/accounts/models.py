aba2cec3842d550f06b31c7f9bfc3c04f068bcf1
Liberbell <liberbell@gmail.com>
2023-10-05 08:40:55 +0900

    models2

--- ClassBaseLoginView/accounts/models.py
+++ ClassBaseLoginView/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
+from django.urls import reverse_lazy
 
 # Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError("Enter Email.")
