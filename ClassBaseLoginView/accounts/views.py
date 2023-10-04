7c005e1987a6c014e1077687da4f7eb4d324ad74
Liberbell <liberbell@gmail.com>
2023-10-05 08:51:56 +0900

    views1

--- ClassBaseLoginView/accounts/views.py
+++ ClassBaseLoginView/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+class RegisterView(CreateView):
+    template_name = "register.html"
