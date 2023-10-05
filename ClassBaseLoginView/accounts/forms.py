4ff8491e9d1207d78c7e0e873de909e8fa32f5b7
Liberbell <liberbell@gmail.com>
2023-10-06 08:26:56 +0900

    forms1

--- ClassBaseLoginView/accounts/forms.py
+++ ClassBaseLoginView/accounts/forms.py
@@ -3,3 +3,7 @@ from .models import Users
 from django.contrib.auth.password_validation import validate_password
 
 class RegistForm(forms.ModelForm):
+    username = forms.CharField(label="Name:")
+    age = forms.IntegerField(label="Age:", min_value=0)
+    email = forms.EmailField(label="Email:")
+    password = forms.CharField(label="Password:", widget=forms.PasswordInput())
