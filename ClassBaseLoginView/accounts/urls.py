3fc3ddeb2ccb957bc716d6c7214c421839ffd37f
Liberbell <liberbell@gmail.com>
2023-10-06 08:39:49 +0900

    urls3

--- ClassBaseLoginView/accounts/urls.py
+++ ClassBaseLoginView/accounts/urls.py
@@ -5,6 +5,7 @@ app_name = "accounts"
 
 urlpatterns = [
     path("home/", HomeView.as_view(), name="home"),
+    path("regist/", RegistUserView.as_view(), name="regist"),
     path("login/", UserLoginView.as_view(), name="login"),
-    path("logout/", UserLogoutView.as_view, name="logout"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
 ]
