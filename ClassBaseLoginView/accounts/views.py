50f2fc8ec95435601040f5f16b80c161f1d1bc90
Liberbell <liberbell@gmail.com>
2023-10-07 13:59:32 +0900

    views3

--- ClassBaseLoginView/accounts/views.py
+++ ClassBaseLoginView/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView, FormView
 from django.views.generic.base import TemplateView, View
 from .forms import RegistForm, UserLoginForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -14,6 +15,11 @@ class RegistUserView(CreateView):
 class UserLoginView(FormView):
     template_name = "user_login.html"
     form_class = UserLoginForm
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(email=email, password=password)
 
 class UserLogoutView(View):
     pass
