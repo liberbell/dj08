ddb7a0f61279d7c13e8d4571722f22191e7bf2ec
Liberbell <liberbell@gmail.com>
2023-10-10 08:34:36 +0900

    01_logging_sample3

--- Logging/01_logging_sample.py
+++ Logging/01_logging_sample.py
@@ -4,3 +4,9 @@ logging.basicConfig(
     level=logging.ERROR, filename="sample.log",
     filemode="w", format="%(asctime)s-%(process)d-%(levelname)s-%(message)s"
 )
+
+logging.debug("debug.log")
+logging.info("info.log")
+logging.warning("warning.log")
+logging.error("error.log")
+logging.critical("critical.log")
