ceeae147a4347b60baa358ff46a5ddd26020b633
Liberbell <liberbell@gmail.com>
2023-10-11 08:18:18 +0900

    02_logging_sample3

--- Logging/02_logging_sample.py
+++ Logging/02_logging_sample.py
@@ -7,6 +7,9 @@ logger.setLevel(logging.DEBUG)
 s_handler = logging.StreamHandler()
 f_handler = logging.FileHandler("logging2.log", encoding="utf-8")
 
+s_handler.setLevel(logging.DEBUG)
+f_handler.setLevel(logging.ERROR)
+
 logging.debug("debug")
 logging.info("info")
 logging.warning("warning")
