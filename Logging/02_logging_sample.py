abbf7b3ce01c9df19fae121713ebae3ae65194c9
Liberbell <liberbell@gmail.com>
2023-10-11 08:15:50 +0900

    02_logging_sample2

--- Logging/02_logging_sample.py
+++ Logging/02_logging_sample.py
@@ -3,3 +3,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 logger.setLevel(logging.DEBUG)
+logging.debug("debug2.log")
+logging.info("info2.log")
+logging.warning("warning2.log")
+logging.error("error2.log")
+logging.critical("critical2.log")
