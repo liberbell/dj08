c115a7d43d3cb4d1ce62ff67626df8cf0163cd2f
Liberbell <liberbell@gmail.com>
2023-10-12 08:51:51 +0900

    04_logging_sample2

--- Logging/04_logging_sample.py
+++ Logging/04_logging_sample.py
@@ -2,3 +2,10 @@ import logging
 import logging.handlers
 
 logger = logging.getLogger(__name__)
+
+r_handler = logging.handlers.RotatingFileHandler(
+    "logs/rotation_file.log",
+    maxBytes=1000,
+    backupCount=5,
+    encoding="utf-8"
+)
