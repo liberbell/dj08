8643d27a63e0154ecdcc057c78dd683261e26abc
Liberbell <liberbell@gmail.com>
2023-10-12 08:36:18 +0900

    03_logging_sample1

--- Logging/03_logging_sample.py
+++ Logging/03_logging_sample.py
@@ -2,3 +2,12 @@ import logging
 import logging.config
 
 logging.config.fileConfig(fname="conf/logger.conf")
+
+logger = logging.getLogger(__name__)
+
+
+logger.debug("Debug log")
+logger.info("Info log")
+logger.warning("Warning log")
+logger.error("Error log")
+logger.critical("Critial log")
