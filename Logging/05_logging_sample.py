f8c35f043762cb4f61b4894eb680596cc0ea2658
Liberbell <liberbell@gmail.com>
2023-10-13 23:50:43 +0900

    05_logging_sample1

--- Logging/05_logging_sample.py
+++ Logging/05_logging_sample.py
@@ -1,4 +1,15 @@
 import logging
 import logging.config
+import time
 
 logging.config.fileConfig(fname="conf/rotation_logger.conf")
+
+logger = logging.getLogger(__name__)
+
+for _ in range(1000):
+    logger.debug("Debug log")
+    logger.info("Info log")
+    logger.warning("Warning log")
+    logger.error("Error log")
+    logger.critical("Critical log")
+    time.sleep(1)
