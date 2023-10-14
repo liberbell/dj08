73d00a058dc595ab988a678ca8cdca37ec8afea9
Liberbell <liberbell@gmail.com>
2023-10-14 14:35:05 +0900

    settings4

--- ClassbasedView/ClassbasedView/settings.py
+++ ClassbasedView/ClassbasedView/settings.py
@@ -146,7 +146,7 @@ LOGGING = {
     'loggers': {
         'application-logger': {
             'handlers': ['console_handler',],
-            'level': 'DEBUG',
+            'level': 'INFO',
             'propagae': False,
         }
     }
