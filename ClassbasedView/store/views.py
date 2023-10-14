c65117cfc5dd2d26d011a75d4e6ce8f174904085
Liberbell <liberbell@gmail.com>
2023-10-14 14:27:42 +0900

    views1

--- ClassbasedView/store/views.py
+++ ClassbasedView/store/views.py
@@ -13,6 +13,9 @@ from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
 import os
+import logging
+
+application_logger = logging.getLogger("application_logger")
 
 # Create your views here.
 class IndexView(View):
