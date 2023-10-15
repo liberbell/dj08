993d2ccd089e2b951ce9026c56e4baf80c6dee33
Liberbell <liberbell@gmail.com>
2023-10-15 11:09:58 +0900

    middleware2

--- ClassbasedView/ClassbasedView/middleware.py
+++ ClassbasedView/ClassbasedView/middleware.py
@@ -3,3 +3,7 @@ from django.utils.deprecation import MiddlewareMixin
 
 application_logger = logging.getLogger("application_logger")
 
+class MyMiddleware(MiddlewareMixin):
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        application_logger.info(request.get_full_path())
