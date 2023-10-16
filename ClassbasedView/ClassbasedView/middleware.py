e1d522a40e098af49e3663cec3c800d216708635
Liberbell <liberbell@gmail.com>
2023-10-16 14:38:40 +0900

    middleware6

--- ClassbasedView/ClassbasedView/middleware.py
+++ ClassbasedView/ClassbasedView/middleware.py
@@ -1,5 +1,6 @@
 import logging
 from django.utils.deprecation import MiddlewareMixin
+import time
 
 application_logger = logging.getLogger("application-logger")
 error_logger = logging.getLogger("error-loger")
@@ -11,4 +12,13 @@ class MyMiddleware(MiddlewareMixin):
 
     def process_exception(self, request, exception):
         error_logger.error(exception, exc_info=True)
-        
+
+
+class PerformanceMiddleware(MiddlewareMixin):
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        start_time = time.time()
+        request.start_time = start_time
+
+    def process_template_response(self, request, response):
+        response_time = time.time() - response.start_time
