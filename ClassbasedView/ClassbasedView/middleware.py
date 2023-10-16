4047ea06e700911ddbe66e11000f59306828f979
Liberbell <liberbell@gmail.com>
2023-10-16 14:21:22 +0900

    middleware1

--- ClassbasedView/ClassbasedView/middleware.py
+++ ClassbasedView/ClassbasedView/middleware.py
@@ -7,3 +7,4 @@ class MyMiddleware(MiddlewareMixin):
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         application_logger.info(request.get_full_path())
+        print(dir(request))
