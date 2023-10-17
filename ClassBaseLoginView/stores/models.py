62c3dbfc7d926a1cf7ec1ed7ba4ce8f046beee82
Liberbell <liberbell@gmail.com>
2023-10-17 12:58:16 +0900

    models2

--- ClassBaseLoginView/stores/models.py
+++ ClassBaseLoginView/stores/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 # Create your models here.
 class ProductTypes(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000)
 
     class Meta:
         db_table = "product_types"
 
         def __str__(self):
             return self.name
+        
+class Manufacturers(models.Model):
+    name = models.CharField(max_length=1000)
+
+    class Meta:
+        db_table = 'manufacturers'
