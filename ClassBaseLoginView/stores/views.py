a3fc5318957cc886c31a58eb6f7b70425e571c3c
Liberbell <liberbell@gmail.com>
2023-10-19 08:38:03 +0900

    views2

--- ClassBaseLoginView/stores/views.py
+++ ClassBaseLoginView/stores/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import (
+    Products,
+)
 import os
 
 
 # Create your views here.
 class ProductListView(LoginRequiredMixin, ListView):
-    model = 
+    model = Products
+    template_name = os.path.join("stores", "product_list.html")
