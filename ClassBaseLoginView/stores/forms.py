bc262d91d2b7301547f15ca30a681b7d0bc114b0
Liberbell <liberbell@gmail.com>
2023-10-26 08:29:37 +0900

    forms2

--- ClassBaseLoginView/stores/forms.py
+++ ClassBaseLoginView/stores/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+from .models import CartItems
 
 class CartUpdateForm(forms.ModelForm):
+    quantity = forms.IntegerField()
+    id = forms.CharField(widget=forms.HiddenInput())
 
     class Meta:
         model = CartItems
+        fields = ["quantity", "id"]
