73d4974b5eb508785682cbeb1f3e7210ae030581
Liberbell <liberbell@gmail.com>
2023-10-26 08:33:08 +0900

    forms3

--- ClassBaseLoginView/stores/forms.py
+++ ClassBaseLoginView/stores/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 from .models import CartItems
+from django.shortcuts import get_object_or_404
 
 class CartUpdateForm(forms.ModelForm):
-    quantity = forms.IntegerField()
+    quantity = forms.IntegerField(label="stock", min_value=1)
     id = forms.CharField(widget=forms.HiddenInput())
 
     class Meta:
         model = CartItems
         fields = ["quantity", "id"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        quantity = cleaned_data.get("quantity")
+        id = cleaned_data.get("id")
+        cart_item = get_object_or_404(CartItems, pk=id)
+            
+        return data
+        
