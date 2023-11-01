3f25a104e12ab20179391ba358fc9f282e2628c5
Liberbell <liberbell@gmail.com>
2023-11-02 08:41:46 +0900

    models4

--- ClassBaseLoginView/stores/models.py
+++ ClassBaseLoginView/stores/models.py
@@ -25,7 +25,7 @@ class ProductsManager(models.Manager):
     def reduce_stock(self, cart):
         for item in cart.cartitems_set.all():
             update_stock = item.product.stock - item.quantity
-            item.prodcut.stock = update_stock
+            item.product.stock = update_stock
             item.product.save()
         
 class Products(models.Model):
