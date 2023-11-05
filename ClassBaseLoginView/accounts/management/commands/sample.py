b493206686c36439fdd677242179f9bc99634eeb
Liberbell <liberbell@gmail.com>
2023-11-05 15:11:21 +0900

    sample6

--- ClassBaseLoginView/accounts/management/commands/sample.py
+++ ClassBaseLoginView/accounts/management/commands/sample.py
@@ -9,6 +9,7 @@ class Command(BaseCommand):
         parser.add_argument("age", type=int)
         parser.add_argument("--birthday", default="2023-01-01")
         parser.add_argument("three_words", nargs=3)
+        parser.add_argument("--active", action="store_true")
         return super().add_arguments(parser)
     
     def handle(self, *args, **options):
@@ -16,5 +17,7 @@ class Command(BaseCommand):
         age = options["age"]
         birthday = options["birthday"]
         three_words = options["three_words"]
+        active = options["active"]
+        print(active)
         print(type(age))
         print(f"name = {name}, age = {age}, birthday = {birthday}, Three words = {three_words}")
