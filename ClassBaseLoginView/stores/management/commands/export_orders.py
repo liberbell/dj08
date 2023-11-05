4acdbd82057d61175a818808d2f7433007f6cc18
Liberbell <liberbell@gmail.com>
2023-11-05 15:21:43 +0900

    export_orders3

--- ClassBaseLoginView/stores/management/commands/export_orders.py
+++ ClassBaseLoginView/stores/management/commands/export_orders.py
@@ -3,10 +3,12 @@ from stores.models import Orders
 from ecsite_project.settings import BASE_DIR
 import os
 import csv
+from datetime import datetime
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         orders = Orders.objects.all()
+        file_path = os.path.join(BASE_DIR, "output", "orders", f"orders_{datetime.now().strftime("%Y%m%d%H%M%S")}")
 
         return super().handle(*args, **options)
