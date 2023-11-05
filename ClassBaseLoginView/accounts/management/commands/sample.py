83a572b1bb303693f846ee88a0fdfb41515f6b27
Liberbell <liberbell@gmail.com>
2023-11-05 15:02:23 +0900

    sample1

--- ClassBaseLoginView/accounts/management/commands/sample.py
+++ ClassBaseLoginView/accounts/management/commands/sample.py
@@ -4,8 +4,8 @@ from django.core.management.base import BaseCommand, CommandParser
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument("name")
-        parser.add_argument("age")
+        parser.add_argument("name", type=str)
+        parser.add_argument("age", type=int)
         parser.add_argument("--birthday")
         return super().add_arguments(parser)
     
@@ -13,4 +13,5 @@ class Command(BaseCommand):
         name = options["name"]
         age = options["age"]
         birthday = options["birthday"]
+        print(type(age))
         print(f"name = {name}, age = {age}, birthday = {birthday}")
