ddb3c2f60655b29963fd1958a34bf26ff2a91957
Liberbell <liberbell@gmail.com>
2023-11-05 15:18:20 +0900

    export_orders2

--- ClassBaseLoginView/stores/management/commands/export_orders.py
+++ ClassBaseLoginView/stores/management/commands/export_orders.py
@@ -1,8 +1,12 @@
 from django.core.management.base import BaseCommand
 from stores.models import Orders
+from ecsite_project.settings import BASE_DIR
+import os
+import csv
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        orders = Orders.objects.all()
 
         return super().handle(*args, **options)
